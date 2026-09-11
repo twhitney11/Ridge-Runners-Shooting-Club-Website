f88257dfcb464b0e410debc27992af426857c5c3
root <root@oafproductions.com>
2026-09-11 04:38:59 +0000

    Initial commit

---
+++ assets/site.js
@@ -0,0 +1,1 @@
+const b=document.querySelector('.nav-toggle'),n=document.querySelector('.site-nav');if(b&&n)b.addEventListener('click',()=>{const o=n.classList.toggle('open');b.setAttribute('aria-expanded',String(o))});
